a4389c90434e5c93ddebc9a8d83e2261979d73aa
Vaishnav Ghenge <62459983+VaishnavGhenge@users.noreply.github.com>
2023-03-05 21:03:22 +0530

    Update getInfo.py

--- UConnectHackathon/Coding_Legos/getInfo.py
+++ UConnectHackathon/Coding_Legos/getInfo.py
@@ -5,9 +5,13 @@ from access_keys import AWS_ACCESS_KEY_ID, AWS_REGION, AWS_SECRET_ACCESS_KEY
 
 
 def getInfo(instance_id):
-    ec2 = boto3.client('ec2', aws_access_key_id=AWS_ACCESS_KEY_ID,
-                       aws_secret_access_key=AWS_SECRET_ACCESS_KEY, region_name=AWS_REGION)
-    instance_response = ec2.describe_instances(InstanceIds=[instance_id])
-    public_dns_address = instance_response['Reservations'][0]['Instances'][0]['PublicIpAddress']
-    print(f"\033[32mInstance public dns: {public_dns_address}\033[0m")
-    
+    try:
+        ec2 = boto3.client('ec2', aws_access_key_id=AWS_ACCESS_KEY_ID,
+                        aws_secret_access_key=AWS_SECRET_ACCESS_KEY, region_name=AWS_REGION)
+        instance_response = ec2.describe_instances(InstanceIds=[instance_id])
+        public_dns_address = instance_response['Reservations'][0]['Instances'][0]['PublicDnsName']
+        print(f"\033[32mInstance public dns: {public_dns_address}\033[0m")
+    except Exception as err:
+        print("\033[31mException occured during getting instance \033[0m")
+        print(f"\033[31mException: {err}\033[31m")
+        return None
