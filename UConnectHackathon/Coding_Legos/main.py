314648f631b10179c772f8f2998d0bddd2c1f9aa
Vaishnav Ghenge <62459983+VaishnavGhenge@users.noreply.github.com>
2023-03-05 21:02:55 +0530

    Update main.py

--- UConnectHackathon/Coding_Legos/main.py
+++ UConnectHackathon/Coding_Legos/main.py
@@ -1,6 +1,7 @@
 # Create an EC2 instance and then attach EBS volume to it(either already created and avialable or create a new one)
 
 from create_instance import create_instance
+from getInfo import getInfo
 
 res = create_instance()
 
@@ -8,4 +9,4 @@ if(not res):
     print("Code exited with code 1")
 else:
     print(f"\033[32mInstance {res.get('instance')} created and attached to {res.get('volume')} successfully!!\033[0m")
-    
+    getInfo(res.get('instance'))
