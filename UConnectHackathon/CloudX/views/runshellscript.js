9950251e7884d9c23f3f87a17531ca55d34213a0
Yash Sarnaik <yash.sarnaik@mitaoe.ac.in>
2023-03-05 23:02:18 +0530

    Delete runshellscript.js

--- UConnectHackathon/CloudX/views/runshellscript.js
+++
@@ -1,9 +0,0 @@
-function name()
-{
-var bucketname = document.getElementById("bucket");
-}
-
-// const { exec } = require('child_process');
-// const shell = require('shelljs')
-
-shell.exec('./bucketveritas.sh');
